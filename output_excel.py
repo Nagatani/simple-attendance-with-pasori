ba39810c4c0f14cb469279a337c8f087e8a007dc
Nagatani <nagatani@work-ark.jp>
2022-10-03 11:28:53 +0900

    Excel出力を各回ごとの出力に変更する

--- output_excel.py
+++ output_excel.py
@@ -5,7 +5,7 @@ import config
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("output_filename", help="出力ファイル名を入力してください。", type=str)
+parser.add_argument("section_id", help="第何回かをパラメータで指定してください。", type=int)
 args = parser.parse_args()
 
 con = sqlite3.connect(config.database_name)
@@ -17,19 +17,17 @@ def main():
   sheet = wb.active
   wb.remove(sheet) # Sheet1の削除
 
-  cur.execute("select section_id from attendance group by section_id")
-  sections = cur.fetchall()
-  for section in sections:
-    # Sheetの作成
-    ws = wb.create_sheet()
-    ws.title = str(section[0])
+  ws = wb.create_sheet() # 第何回かのシートを作成
+  ws.title = str(args.section_id)
 
-    cur.execute("select id, card_id, student_id, create_datetime, update_datetime from attendance where section_id = ? order by student_id", (section[0], ))
-    attended = cur.fetchall()
-    for row in attended:
-      ws.append(row)
+  cur.execute("select id, card_id, student_id, forgot_card, create_datetime, update_datetime from attendance where section_id = ? order by student_id", (args.section_id, ))
 
-  wb.save(args.output_filename)
+  ws.append(['ROWNUMBER', 'カードID', '学籍番号', 'カード忘れ', '登録日時', '更新日時'])
+  attended = cur.fetchall()
+  for row in attended:
+    ws.append(row)
+
+  wb.save(f"Attend_{ args.section_id }.xlsx")
 
 
 if __name__ == "__main__":
