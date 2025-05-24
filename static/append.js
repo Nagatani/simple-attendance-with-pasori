d5f947c617c740639b8621bff8ddd13fdfb3f0fc
Nagatani <nagatani@work-ark.jp>
2025-05-25 01:43:27 +0900

    Revert "Refactor: Add JSDoc, modularize JS, and enhance felica.js readability."
    This reverts commit 73286243702ebddc32c4feb8687b245915a72f57.

---
+++ static/append.js
@@ -0,0 +1,220 @@
+// 機能追加
+
+let inputIdm = document.getElementById('input_idm')
+let inputStudntId = document.getElementById('input_student_id')
+let favDialog = document.getElementById('favDialog')
+let attendedList = document.getElementById('attendedList')
+let hedding1 = document.getElementById('hedding1')
+
+/**
+ * 出席登録
+ * @param {Felica IDM} card_id 
+ */
+ const attend = (card_id) => {
+
+  let formData = new FormData()
+  formData.append('card_id', card_id) // cardId のみ送信する 学籍番号はデータベースから取得する
+  console.log(formData)
+  
+  fetch('/attend', {method: 'POST', body: formData}).then(response => {
+    return response.json().then(data => {
+      if (response.ok) {
+        console.log(data)
+        if (data.status === 'error') {
+          favDialog.showModal()
+          inputStudntId.focus()
+        } else {
+          // 出席表示
+          addAttend(data.student_id, true)
+        }
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+  })
+}
+
+
+/**
+ * 新規生徒情報をサーバー側に送信する
+ */
+const register = (card_id, student_id) => {
+
+  let formData = new FormData()
+  formData.append('card_id', card_id)
+  formData.append('student_id', student_id)
+  //console.log(formData)
+  
+  fetch('/register', {method: 'POST', body: formData}).then(response => {
+    return response.json().then(data => {
+      if (response.ok) {
+        console.log(data)
+        if (data.status === 'error') {
+          //favDialog.showModal()
+          inputStudntId.focus()
+        } else {
+          // 正常登録が終わったはずなので、dialogを閉じて学籍番号の入力欄をクリアして出席表示
+          favDialog.close()
+          inputStudntId.value = ''
+
+          // 出席表示
+          addAttend(data.student_id, true)
+        }
+
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+  })
+}
+
+
+/**
+ * ダイアログ上で学籍番号を入力(バーコードリーダーでスキャン)した際に発火させるキーイベント
+ */
+inputStudntId.addEventListener('keypress', (ev) => {
+  if (ev.key === 'Enter' && inputStudntId.value !== '') {
+    // console.log('Enterが押されました。')
+    // keypressイベントの消去
+    ev.preventDefault()
+    ev.stopPropagation()
+    ev.stopImmediatePropagation()
+
+    console.log(inputIdm.value, inputStudntId.value)
+    register(inputIdm.value, inputStudntId.value)
+  }  
+})
+
+
+let attends = []
+let students = []
+
+/**
+ * 出席情報を取得して表示する（エラーにより再起動した際の対策）
+ */
+const showAllAttendance = () => {
+  attends = []
+  students = []
+  fetch('/get-attended').then(response => {
+    return response.json().then(data => {
+      if (response.ok) {
+        console.log(data)
+        attends = data
+
+        while (attendedList.firstChild) {
+          attendedList.removeChild(attendedList.firstChild)
+        }
+        attends.forEach(element => {
+          if (element.length >= 2) {
+            addAttend(element[1], false)
+          }
+        })
+        hedding1.textContent = ``
+
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+  })
+}
+
+
+/**
+ * 出席表示
+ * 
+ * @param {student_id} student_id 
+ */
+const addAttend = (student_id, playsound) => {
+
+  if (playsound) {
+    const sound = document.getElementById('attend_sound')
+    sound.currentTime = 0
+    sound.muted = false
+    sound.play()
+  }
+
+  if (students.includes(student_id)) {
+    hedding1.textContent = `出席済: ${student_id}`
+    inputIdm.value = ''
+    return
+  }
+
+  let label = document.createElement('button')
+  label.classList.add('uk-button')
+  label.classList.add('uk-button-default')
+  label.textContent = student_id
+  attendedList.insertBefore(label, attendedList.firstChild)
+
+  hedding1.textContent = `出席: ${student_id}`
+  students.push(student_id)
+  inputIdm.value = ''
+}
+
+let forgotCardLink = document.getElementById('forgotCard')
+let forgotCardDialog = document.getElementById('forgotCardDialog')
+let forgotCardDialogMessage = document.getElementById('forgotCardDialogMessage')
+
+forgotCardLink.addEventListener('click', (ev) => {
+  forgotCardDialog.showModal()
+
+  ev.preventDefault()
+  ev.stopPropagation()
+  ev.stopImmediatePropagation()
+})
+
+let inputForgotStudntId = document.getElementById('input_forgot_student_id')
+
+/**
+ * ダイアログ上で学籍番号を入力(バーコードリーダーでスキャン)した際に発火させるキーイベント
+ */
+ inputForgotStudntId.addEventListener('keypress', (ev) => {
+  if (ev.key === 'Enter' && inputForgotStudntId.value !== '') {
+    // keypressイベントの消去
+    ev.preventDefault()
+    ev.stopPropagation()
+    ev.stopImmediatePropagation()
+
+    console.log(inputForgotStudntId.value)
+    register_forgot(inputForgotStudntId.value)
+  }  
+})
+
+
+/**
+ * カード忘れ情報をサーバー側に送信する
+ */
+ const register_forgot = (student_id) => {
+
+  let formData = new FormData()
+  formData.append('student_id', student_id)
+  
+  fetch('/forgot_card', {method: 'POST', body: formData}).then(response => {
+    return response.json().then(data => {
+      if (response.ok) {
+        console.log(data)
+        if (data.status === 'error') {
+          //favDialog.showModal()
+          forgotCardDialogMessage.textContent = data.message
+          inputForgotStudntId.focus()
+        } else {
+          // 正常登録が終わったはずなので、dialogを閉じて学籍番号の入力欄をクリアして出席表示
+          forgotCardDialog.close()
+          inputForgotStudntId.value = ''
+
+          // 出席表示
+          addAttend(data.student_id, false)
+        }
+
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+  })
+}
+
+// 初回実行
+showAllAttendance()
